91fa267b7eb3d731901dd3402b450abf8513857d
yisus1002 <yisusgarcia1002@gmail.com>
2023-01-18 21:43:07 -0500

    Se cambio la variable global

--- src/app/services/parent/parent.service.ts
+++ src/app/services/parent/parent.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ParentService {
 
-  public API_URL: string = 'https://iot-restserver-production.up.railway.app/';
+  // public API_URL: string = 'https://iot-restserver-production.up.railway.app/';
+  public API_URL: string = 'https://iotagroiot-production.up.railway.app/';
   constructor() { }
 }
